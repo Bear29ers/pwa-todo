bb641acbbe8fb9d4db9172dbea5083a433ee9c06
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-29 00:52:38 +0900

    remove: :fire: remove husky and lint-staged

--- .lintstagedrc.cjs
+++
@@ -1,7 +0,0 @@
-const config = {
-  '**/*.{js,jsx,ts,tsx}': 'eslint',
-  '**/*.{js,jsx,ts,tsx,json}': 'prettier --write',
-  '**/*.{html,jsx,tsx}': 'markuplint --fix',
-};
-
-module.exports = config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-<div>
+const App = () => {
+  return (
+    <div>
       <form onSubmit={(e) => e.preventDefault()}>
         <input type='text' value='' onChange={(e) => e.preventDefault()} />
         <input type='submit' value='追加' onSubmit={(e) => e.preventDefault()} />
       </form>
     </div>
+  );
+};
+
+export default App;
+
