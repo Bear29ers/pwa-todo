2b85450994c0019ee820070ec191559f9da07079
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-28 22:08:55 +0900

    dep-add: :heavy_plus_sign: add eslint plugins and update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,140 @@
-module.exports = {
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+
+const config = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-}
+  plugins: ['import', 'unused-imports', 'jsx-a11y', '@typescript-eslint', 'react-refresh'],
+  rules: {
+    /* eslint */
+    'no-unused-vars': 'off',
+    'arrow-body-style': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSEnumDeclaration',
+        message: 'DO NOT DECLARE ENUM',
+      },
+    ],
+    /* typescript */
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-expect-error': 'allow-with-description',
+        'ts-ignore': false,
+        'ts-nocheck': false,
+        'ts-check': false,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    /* react */
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-unknown-property': ['error', { ignore: ['space'] }],
+    /* react-refresh */
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    /* import */
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'groups': ['builtin', 'external', 'internal', ['parent', 'sibling'], 'object', 'type', 'index'],
+        'newlines-between': 'always',
+        'pathGroupsExcludedImportTypes': ['builtin'],
+        'pathGroups': [
+          {
+            pattern: 'react/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '{@/app/**,@/hooks/**,@/libs/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '{@/components/**,@/pages/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/const/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        'alphabetize': {
+          order: 'asc',
+        },
+      },
+    ],
+    /* unused-imports */
+    'unused-imports/no-unused-imports': 'error',
+  },
+};
+
+module.exports = config;
