4f345df7d4e4aa7a59f8b3bcb6d14a113e2a6bb3
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-29 18:59:22 +0900

    comments: :bulb: add JSDoc for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App.tsx';
-import './index.css';
-
-const root = createRoot(document.getElementById('root') as Element);
-
-root.render(
-  <StrictMode>
+<StrictMode>
     <App />
   </StrictMode>
-);
