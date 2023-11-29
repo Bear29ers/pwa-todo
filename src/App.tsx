d4b4d0828eb4b885465339184c2e1a2fc44d7f24
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-29 22:28:57 +0900

    feature: :sparkles: bind callback with eventlistener

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 // "Todo"型の定義
 type Todo = {
@@ -7,19 +7,33 @@ type Todo = {
 
 const App = () => {
   const [text, setText] = useState('');
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [_, setTodos] = useState<Todo[]>([]);
+
+  // todosを更新する
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    // スプレッド構文で元のtodos配列のすべての要素を列挙する
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+    setText('');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type='text'
-          // textステートが持っている入力中のテキストの値をvalueとして表示
-          value={text}
-          // onChangeイベント（= 入力テキストの変化）をtextステートに反映する
-          onChange={(e) => setText(e.target.value)}
-        />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}>
+        <input type='text' value={text} onChange={(e) => handleChange(e)} />
         <input type='submit' value='追加' onSubmit={(e) => e.preventDefault()} />
       </form>
 
