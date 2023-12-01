aaab54c0e2af4af962521136fad8942347a23023
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-01 21:41:39 +0900

    feature: :sparkles: add the ability to filter todos

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ type Todo = {
   removed: boolean;
 };
 
+type Filter = 'all' | 'checked' | 'unchecked' | 'removed';
+
 const App = () => {
   /**
    * コンポーネントに状態を「記憶」させるためのuseState
@@ -15,6 +17,7 @@ const App = () => {
    */
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>('all');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     /**
@@ -85,18 +88,56 @@ const App = () => {
     });
   };
 
+  // フィルターを変更する
+  const handleSort = (argFilter: Filter) => {
+    setFilter(argFilter);
+  };
+
+  // todoをフィルターする
+  const filteredTodos = todos.filter((todo) => {
+    // filter stateの値に応じて異なる内容の配列を返す
+    switch (filter) {
+      case 'all':
+        return !todo.removed;
+      case 'checked':
+        return todo.checked && !todo.removed;
+      case 'unchecked':
+        return !todo.checked && !todo.removed;
+      case 'removed':
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
+
   return (
     <div>
+      <select defaultValue="all" onChange={(e) => handleSort(e.target.value as Filter)}>
+        <option value="all">すべてのタスク</option>
+        <option value="checked">完了したタスク</option>
+        <option value="unchecked">現在のタスク</option>
+        <option value="removed">ごみ箱</option>
+      </select>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
         }}>
-        <input type='text' value={text} onChange={(e) => handleChange(e)} />
-        <input type='submit' value='追加' onSubmit={(e) => e.preventDefault()} />
+        <input
+          type="text"
+          disabled={filter === 'checked' || filter === 'removed'}
+          value={text}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="submit"
+          disabled={filter === 'checked' || filter === 'removed'}
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
       </form>
       <ul>
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           /**
            * Reactはリストをレンダーする際、どのアイテムに変更が加えられたかを特定する必要があるため、
            * 変更・追加・削除・並び替えを検知するためには、リストの各項目を特定する一意な識別子（key）が必要
@@ -105,18 +146,18 @@ const App = () => {
           return (
             <li key={todo.id}>
               <input
-                type='checkbox'
+                type="checkbox"
                 disabled={todo.removed}
                 checked={todo.checked}
                 onChange={() => handleCheck(todo.id, !todo.checked)}
               />
               <input
-                type='text'
+                type="text"
                 disabled={todo.checked || todo.removed}
                 value={todo.value}
                 onChange={(e) => handleEdit(todo.id, e.target.value)}
               />
-              <button type='button' onClick={() => handleRemove(todo.id, !todo.removed)}>
+              <button type="button" onClick={() => handleRemove(todo.id, !todo.removed)}>
                 {todo.removed ? '復元' : '削除'}
               </button>
             </li>
