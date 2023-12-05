d60571fd5269f532692613115486b913acc1f0ae
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-05 23:56:00 +0900

    wip: :construction: creating SideBar

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,12 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import Avatar from '@mui/material/Avatar';
+import Drawer from '@mui/material/Drawer';
+import Icon from '@mui/material/Icon';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { indigo, pink } from '@mui/material/colors';
+import styled from '@mui/material/styles';
+
+import pjson from '../package.json';
+
+import type { Theme } from '@mui/material/styles';
+
 type Props = {
+  drawerOpen: boolean;
+  onToggleDrawer: () => void;
   onSort: (filter: Filter) => void;
 };
 
-export const SideBar = ({ onSort }: Props) => (
-  <select defaultValue="all" onChange={(e) => onSort(e.target.value as Filter)}>
-    <option value="all">すべてのタスク</option>
-    <option value="checked">完了したタスク</option>
-    <option value="unchecked">現在のタスク</option>
-    <option value="removed">ごみ箱</option>
-  </select>
+// ドロワー内リストの幅をカスタマイズ
+const DrawerList = styled('div')(() => ({
+  width: 250,
+}));
+
+// ドロワーヘッダーのサイズ・色などをカスタマイズ
+const DrawerHeader = styled('div')(() => ({
+  height: 150,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '1em',
+  backgroundColor: indigo[500],
+  color: '#ffffff',
+  fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, sans-serif',
+}));
+
+// ヘッダー内に表示するアバターのカスタマイズ
+const DrawerAvatar = styled(Avatar)(({ theme }: { theme: Theme }) => ({
+  backgroundColor: pink[500],
+  width: theme.spacing(6),
+  height: theme.spacing(6),
+}));
+
+export const SideBar = ({ drawerOpen, onToggleDrawer, onSort }: Props) => (
+  <Drawer variant="temporary" open={drawerOpen} onClose={onToggleDrawer}>
+    <DrawerList role="presentation" onClick={onToggleDrawer}>
+      <DrawerHeader>
+        <DrawerAvatar>
+          <Icon>create</Icon>
+        </DrawerAvatar>
+        <p>TODO v{pjson.version}</p>
+      </DrawerHeader>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton aria-label="list-all" onClick={() => onSort('all')}>
+            <ListItemIcon>
+              <Icon>subject</Icon>
+            </ListItemIcon>
+            <ListItemText secondary="すべてのタスク" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </DrawerList>
+  </Drawer>
 );
